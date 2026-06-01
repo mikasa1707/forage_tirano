cb67823040082399663e8cc399288df5697238da
mikasa1707 <raveroson73@gmail.com>
2026-06-01 15:00:56 +0300

    back

--- backend-forage/src/main.ts
+++ backend-forage/src/main.ts
@@ -6,7 +6,10 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'http://localhost:4200', // Angular
+    origin: [
+      'http://localhost:4200',
+      'https://radiant-beauty-production-903e.up.railway.app'
+    ],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: true,
   });
