bd4e132b5945cb8c6b4b9b1e48f8df98db5f08c4
mikasa1707 <raveroson73@gmail.com>
2026-06-01 13:38:15 +0300

    m

--- backend-forage/src/main.ts
+++ backend-forage/src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  console.log('DB URL:', process.env.DATABASE_URL);
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
