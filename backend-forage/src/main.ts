a5f6d025273a62a9f043bd233fd8700a5c31407e
mikasa1707 <raveroson73@gmail.com>
2026-06-02 21:33:04 +0300

    Correction

--- backend-forage/src/main.ts
+++ backend-forage/src/main.ts
@@ -5,21 +5,23 @@ import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
   console.log('DB URL:', process.env.DATABASE_URL);
-  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  const app = await NestFactory.create(AppModule);
 
   app.enableCors({
     origin: [
       'http://localhost:4200',
-      'https://radiant-beauty-production-903e.up.railway.app'
+      'https://radiant-beauty-production-903e.up.railway.app',
+      'https://foragetirano-production.up.railway.app'
     ],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: true,
+  allowedHeaders: ['Content-Type', 'Authorization'],
   });
 
   // // ✅ rendre uploads accessible
-  app.useStaticAssets(join(__dirname, '..', 'uploads'), {
-    prefix: '/uploads',
-  });
+  // app.useStaticAssets(join(__dirname, '..', 'uploads'), {
+  //   prefix: '/uploads',
+  // });
 
   await app.listen(process.env.PORT || 3000);
 }
